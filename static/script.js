7357ddf1316cd3ec23f7f3d49da421c1fc498c34
igoyrock1 <Google@gmail.com>
2025-05-01 18:34:26 +0300

    66668888

--- static/script.js
+++ static/script.js
@@ -1,11 +1,15 @@
-
-
-alert('Welcome to my chat')
-
 const socket = io();
 const messages = document.getElementById('messages');
 const form = document.getElementById('form');
 const input = document.getElementById('input');
+socket.on('all_messages', function(msgArray) {
+  msgArray.forEach(msg => {
+    let item = document.createElement('li');
+    item.textContent = msg.login + ': ' + msg.content;
+    messages.appendChild(item);
+  });
+  window.scrollTo(0, document.body.scrollHeight);
+});
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   if (input.value) {
@@ -14,11 +18,12 @@ form.addEventListener('submit', function(e) {
   }
 });
 socket.on('message', function(msg) {
-    var item = document.createElement('li');
+    let item = document.createElement('li');
     item.textContent = msg;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
 });
+
 function changeNickname() {
   let nickname = prompt('Choose your nickname');
   if (nickname) {
